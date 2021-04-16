f633cbb13c559ddaed001b869718a7ed707440eb
williamtovar <williamtovar93@gmail.com>
2021-04-15 22:30:08 -0500

    Edicion minima UI

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-//Defino Variables
+//Defino mis buttons y los vinculo 
 var btn_piedra = document.getElementById("Piedra");
 var btn_papel = document.getElementById("Papel");
 var btn_tijeras = document.getElementById("Tijeras");
@@ -23,27 +23,32 @@ btn_iniciar.onclick = function () {
   Opcion_pc = num_aleatorio();
   console.log(Opcion_pc, " opt pc");
   display_opcion_pc(Opcion_pc);  
-  setTimeout(game(Opcion_jugador, Opcion_pc),3000);  
+  game(Opcion_jugador, Opcion_pc);  
 };
 btn_piedra.onclick = function () {
   Opcion_jugador = 0;
   img_user.src = "./assets/rockPPTLS.png";
+  btn_iniciar.disabled = false;
 };
 btn_papel.onclick = function () {
   Opcion_jugador = 1;
   img_user.src = "./assets/paperPPTLS.png";
+  btn_iniciar.disabled = false;
 };
 btn_tijeras.onclick = function () {
   Opcion_jugador = 2;
   img_user.src = "./assets/scissorsPPTLS.png";
+  btn_iniciar.disabled = false;
 };
 btn_lagarto.onclick = function () {
   Opcion_jugador = 3;
   img_user.src = "./assets/lizardPPTLS.png";
+  btn_iniciar.disabled = false;
 };
 btn_spock.onclick = function () {
   Opcion_jugador = 4;
   img_user.src = "./assets/spockPPTLS.png";
+  btn_iniciar.disabled = false;
 };
 
 switch (Menu) {
@@ -79,6 +84,7 @@ function game(opcion_jugador, opcion_pc) {
   var escenario_9; //Spock 4 gana Tijeras 2
   var escenario_10; //Spock 4 gana Piedra 0
   var empate;
+  /* --------------------Con IF -------------------
   if (opcion_jugador === opcion_pc) {
     empate = true;
   } else if (opcion_jugador === 0 && opcion_pc === 2) {
@@ -102,6 +108,44 @@ function game(opcion_jugador, opcion_pc) {
   } else if (opcion_jugador === 4 && opcion_pc === 0) {
     escenario_10 = true;
   }
+*//*---------------Con Switch-----------------*/
+
+switch(true){
+  case (opcion_jugador === opcion_pc):
+    empate = true;
+    break;
+  case (opcion_jugador === 0 && opcion_pc === 2):
+    escenario_1 = true;
+    break;
+  case (opcion_jugador === 1 && opcion_pc === 0):
+    escenario_2 = true;
+    break;
+  case (opcion_jugador === 2 && opcion_pc === 1):
+    escenario_3 = true;
+    break;
+  case (opcion_jugador === 0 && opcion_pc === 3):
+    escenario_4 = true;
+    break;
+  case (opcion_jugador === 1 && opcion_pc === 4):
+    escenario_5 = true;
+    break;
+  case (opcion_jugador === 2 && opcion_pc === 3):
+    escenario_6 = true;
+    break;
+  case (opcion_jugador === 3 && opcion_pc === 4):
+    escenario_7 = true;
+    break;
+  case (opcion_jugador === 3 && opcion_pc === 1): 
+    escenario_8 = true;
+    break;
+  case  (opcion_jugador === 4 && opcion_pc === 2):
+    escenario_9 = true;
+    break;
+  case (opcion_jugador === 4 && opcion_pc === 0):
+    escenario_10 = true;
+    break;
+}
+
 
   if (
     escenario_1 ||
